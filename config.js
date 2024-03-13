b7d6b2d07eb900ea7c8f344198d48d0ca03262e3
unknown <rommel.aquino.mendiola@gmail.com>
2024-03-13 09:53:46 +0800

    Fixed Login

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
-var portalCfg = {
-    'portal_name': 'Renz PiSo WiFi',
-    'host': 'https://waspradi.us',
-    'rates': [
-        { "rate_name": "Php", "rate": "5", "time": "4 Hours" },
-        { "rate_name": "Php", "rate": "10", "time": "12 Hours" },
+const data = {
+    "portal_name": "RizBeth WiFi",
+    "host": "https://waspradi.us",
+    "rates": [
+        {
+            "rate_name": "Php",
+            "rate": "5",
+            "time": "4 Hours"
+        },
+        {
+            "rate_name": "Php",
+            "rate": "10",
+            "time": "12 Hours"
+        }
+    ],
+    "slides": [
+        "slide1.png",
     ]
-};
+}
